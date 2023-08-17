6f7c8a857ed5226dde825f69458e72f308cbed62
FredNeves95 <fredbneves95@gmail.com>
2023-08-16 22:04:58 -0300

    feat: create base routes

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { router } from './router';
 
 mongoose.connect('mongodb://localhost:27017')
   .then(() => {
@@ -10,6 +11,8 @@ mongoose.connect('mongodb://localhost:27017')
     app.listen(port, () => {
       console.log(`Server is running on http://localhost:${3001}`);
     });
+
+    app.use(router);
   })
   .catch(()=> console.log('Erro ao conectar no MongoDB'));
 
