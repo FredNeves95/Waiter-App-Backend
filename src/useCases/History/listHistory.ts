55eea38fdf7261ce6f61eff48457abd9ff60d620
FredNeves95 <fredbneves95@gmail.com>
2023-09-07 18:27:57 -0300

    feat: history with pagination

--- src/useCases/History/listHistory.ts
+++ src/useCases/History/listHistory.ts
@@ -3,12 +3,18 @@ import { Request, Response } from 'express';
 import { History } from '../../models/History';
 
 export async function listHistory(req: Request, res: Response){
+  const { page = 1, limit = 10 } = req.query;
   try {
     const history = await History.find()
-      .sort({ archivedAt: 1 })
-      .populate('products.product');
+      .sort({ archivedAt: -1 })
+      .populate('products.product')
+      .limit(Number(limit) * 1)
+      .skip((Number(page) - 1) * Number(limit));
 
-    res.json(history);
+    const totalItems = await History.count();
+
+    res.json({ items: history, total: totalItems, totalPages: Math.ceil(totalItems / Number(limit)),
+      currentPage: page });
   } catch (error) {
     res.sendStatus(500);
   }
